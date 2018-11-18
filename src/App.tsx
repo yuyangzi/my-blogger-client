04516fecb025eab5347e78a8f38cd5885a5e7f39
wangyiming <wangyimig@icloud.com>
2018-11-19 00:42:51 +0800

    chore: 使用prettier格式化代码, 使用husky执行Git生命周期钩子

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HomePage from "./pages/home/home.page";
-import './config/config'
+import HomePage from './pages/home/home.page';
+import './config/config';
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="App container sidebar-position-left page-home">
-                    <Switch>
-                        <Route exact={true} path="/list" component={HomePage}/>
-                        <Redirect to={'/list'}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App container sidebar-position-left page-home">
+          <Switch>
+            <Route exact={true} path="/list" component={HomePage} />
+            <Redirect to={'/list'} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/config/font-config/font-config.tsx
+++ src/config/font-config/font-config.tsx
@@ -1,15 +1,34 @@
-import {library} from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-    faArchive,
-    faCalendar, faChevronLeft, faChevronRight, faClock,
-    faEye, faFileWord, faFolder,
-    faHeartbeat,
-    faHome,
-    faSearch,
-    faTags,
-    faTh
-} from '@fortawesome/free-solid-svg-icons'
+  faArchive,
+  faCalendar,
+  faChevronLeft,
+  faChevronRight,
+  faClock,
+  faEye,
+  faFileWord,
+  faFolder,
+  faHeartbeat,
+  faHome,
+  faSearch,
+  faTags,
+  faTh
+} from '@fortawesome/free-solid-svg-icons';
 
-const fontIcons = [faHome, faTh, faArchive, faTags, faHeartbeat, faSearch, faEye, faCalendar, faFileWord, faClock, faFolder, faChevronLeft, faChevronRight];
+const fontIcons = [
+  faHome,
+  faTh,
+  faArchive,
+  faTags,
+  faHeartbeat,
+  faSearch,
+  faEye,
+  faCalendar,
+  faFileWord,
+  faClock,
+  faFolder,
+  faChevronLeft,
+  faChevronRight
+];
 
 library.add(...fontIcons);
