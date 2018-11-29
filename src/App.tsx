31b9815f38367bac84e48ef11a31a5d8a2d58523
wangyiming <wangyimig@icloud.com>
2018-11-29 23:42:09 +0800

    chore: 升级依赖npm依赖,添加enzyme进行测试断言

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import './config/config'
 import FooterPage from './pages/footer/footer.page'
@@ -7,17 +7,17 @@ import HomePage from './pages/home/home.page'
 import HeaderPage from './pages/header/header.page'
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="App container sidebar-position-left page-home">
-                    <HeaderPage />
-                    <HomePage />
-                    <FooterPage />
-                </div>
-            </BrowserRouter>
-        )
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App container sidebar-position-left page-home">
+          <HeaderPage />
+          <HomePage />
+          <FooterPage />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App
